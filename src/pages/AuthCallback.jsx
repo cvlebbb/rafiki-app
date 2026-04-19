ea8a311274139cc269ba53e6153809669af44212
cvlebbb <cvlebwhaka@gmail.com>
2026-04-19 13:23:22 +0300

    Update AuthCallback.jsx

--- src/pages/AuthCallback.jsx
+++ src/pages/AuthCallback.jsx
@@ -1,7 +1,7 @@
 // src/pages/AuthCallback.jsx
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../lib/supabase"; // Adjust path to your supabase client
+import { supabase } from "../lib/supabaseClient"; // Adjust path to your supabase client
 import { useAuthStore } from "../stores/useAuthStore";
 
 export default function AuthCallback() {
